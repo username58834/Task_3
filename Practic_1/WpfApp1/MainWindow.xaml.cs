b7449f8d9f37f101e79670d629bf6224fbeb5d64
Elvira <e.y.volobuieva@student.khai.edu>
2025-07-11 01:25:50 +0300

    Add main application functions

---
+++ Practic_1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnExit_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void BtnCount_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            double c = double.Parse(Txt_C.Text);
+            double d = double.Parse(Txt_D.Text);
+
+            double res = 2 * c + 82 * d;
+            Txt_Result.Text = res.ToString();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+}
